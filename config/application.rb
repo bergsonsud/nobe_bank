2bc09bdcab986fb41be4e91fde0f1e92450d3e1f
bergsonsud <bergsonsud@gmail.com>
2021-01-07 03:46:52 -0300

    Precompile assets

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module NobeBank
     config.load_defaults 6.0
 
     config.time_zone = 'Brasilia'
+    config.assets.initialize_on_precompile = false
     config.i18n.default_locale = :'pt-BR'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
